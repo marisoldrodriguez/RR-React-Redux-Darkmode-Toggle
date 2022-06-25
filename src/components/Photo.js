2b59075ea9190c8a2dbde171a7af5bb5e4e612ac
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-06-25 13:46:09 -0400

    meets basic criteria for Darkmode-Toggle activity in module 10

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 function Photo() {
+    const mode = useSelector((state) => state.mode)
 
     return (
-        <div className="Photo">
+        <div className="Photo" style={{'backgroundColor': mode.color3}}>
             <img src='https://placekitten.com/200/300' alt="author" />
         </div>
     )
